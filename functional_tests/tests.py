5e5501ed4285ce8f9b88f6f4542ef63374c9a0be
AntonEsipow <esipow.anton99@gmail.com>
2021-11-22 14:54:18 +0300

    В ФТ добавлена проверка ошибок, связанных со временем ожидания и убраны time.sleep

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -16,11 +19,20 @@ class NewVisitorTest(LiveServerTestCase):
         """Демонтаж"""
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        """подтверждение строки в таблице теста"""
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        """ожидать строку в таблице списка"""
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест: можно начать список и получить его позже"""
@@ -38,18 +50,18 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('Купить вагонку')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        # time.sleep(1)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Купить гвозди')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        # time.sleep(1)
 
         # table = self.browser.find_element_by_id('id_list_table')
         # rows = table.find_elements_by_tag_name('tr')
 
-        self.check_for_row_in_list_table('1: Купить вагонку')
-        self.check_for_row_in_list_table('2: Купить гвозди')
+        self.wait_for_row_in_list_table('1: Купить вагонку')
+        self.wait_for_row_in_list_table('2: Купить гвозди')
 
         self.fail('Закончить тест!')
 
