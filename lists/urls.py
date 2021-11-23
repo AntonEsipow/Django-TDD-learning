6467dcefced823aee2932a7fa4db66c45d735a57
AntonEsipow <esipow.anton99@gmail.com>
2021-11-23 15:45:14 +0300

    Новый URL, view, template для отображения списков

--- lists/urls.py
+++ lists/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_page, name='home')
+    path('', views.home_page, name='home'),
+    path('lists/all/', views.view_list, name='view_list'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -7,7 +7,11 @@ def home_page(request):
     """Домашняя страница"""
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
-    items = Item.objects.all()
+        return redirect('/lists/all/')
+    return render(request, 'home.html')
 
-    return render(request, 'home.html', {'items': items})
+
+def view_list(request):
+    """представление списка"""
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
